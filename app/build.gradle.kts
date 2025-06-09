2457a6e65ea787e7e36af39ad6cd02ce1e2b56b1
won2eu <hasw3314@gmail.com>
2025-06-09 21:40:53 +0900

    2025-05-21 위치인증화면, 채팅화면 더함. 그리고 홈화면 로그인화면 대거 수정함. 스플래시 화면 추가해야 됨 그리고 홈화면에 넣을 이미지들 구해서 drawble에 추가해야됨

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
