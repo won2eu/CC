763be25b0008a9d5f1358684ae89d49485ae1dfc
won2eu <hasw3314@gmail.com>
2025-06-20 02:08:42 +0900

    2025-05-21 일단 위치인증 화면까지 연결했음.

--- app/src/main/java/com/example/ccgi/MainActivity.kt
+++ app/src/main/java/com/example/ccgi/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.ccgi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.btn_start_matching)
+        startButton.setOnClickListener {
+            val intent = Intent(this, VerificationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
